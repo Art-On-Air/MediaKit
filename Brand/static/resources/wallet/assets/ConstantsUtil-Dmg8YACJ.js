5000c3426ef612f26776b2b39ff43bf15ebf18ba
razzzzee <rahulyadav2005@live.com>
2025-05-22 01:31:13 +0530

    Update ConstantsUtil-Dmg8YACJ.js

--- Brand/static/resources/wallet/assets/ConstantsUtil-Dmg8YACJ.js
+++ Brand/static/resources/wallet/assets/ConstantsUtil-Dmg8YACJ.js
@@ -1,1 +1,1 @@
-const s=/[.*+?^${}()|[\]\\]/gu,e=/[0-9,.]/u,t="https://reown.com";export{t as R,e as n,s};
+const s=/[.*+?^${}()|[\]\\]/gu,e=/[0-9,.]/u,t="https://artonair.world";export{t as R,e as n,s};
